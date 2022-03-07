595d34108e45fdb125244bc5d8efd5fd249d8cd4
Alejandro Costa <alejandroesfc@gmail.com>
2022-03-07 07:33:59 -0300

    Defines and implements a generic class for Employee

---
+++ src/dev/alejandrocosta/model/Employee.java
@@ -0,0 +1,87 @@
+package dev.alejandrocosta.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * Defines a generic class for employees. This class will be used in demonstrations of different topics using the Java language.
+ * @author alejandro_costa
+ * @version 0.1 2022-03-07
+ */
+public class Employee 
+{
+	private String name;
+	private double salary;
+	private LocalDate hireDay;
+	
+	public Employee(String nome, double salary, int year, int month, int day)
+	{
+		this.name = nome;
+		this.salary = salary;
+		this.hireDay = LocalDate.of(year, month, day);
+	}
+
+	/**
+	 * Method to get the name of the employee.
+	 * @return a String with the name of the Employee.
+	 * @since 0.1
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Method to get the salary of the employee.
+	 * @return a Double with the salary of the Employee.
+	 * @since 0.1
+	 */
+	public double getSalary() {
+		return salary;
+	}
+
+	/**
+	 * Method to get the date in which the employee was hired.
+	 * @return a LocalDate with the date in which the employee was hired.
+	 * @since 0.1
+	 */
+	public LocalDate getHireDay() {
+		return hireDay;
+	}
+	
+	/**
+	 * Method to raise the salary of the employee by some percentage.
+	 * @param byPercent A double indicating the percentage with which the salary of the employee should be raised.
+	 * @since 0.1
+	 */
+	public void raiseSalary(double byPercent)
+	{
+		double raise = this.salary * byPercent / 100;
+		this.salary += raise;
+	}
+
+	@Override
+	public int hashCode() 
+	{
+		return Objects.hash(hireDay, name, salary);
+	}
+
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(hireDay, other.hireDay) && Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(salary) == Double.doubleToLongBits(other.salary);
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name = " + name + ", salary = " + salary + ", hireDay = " + hireDay + "]";
+	}	
+	
+}
